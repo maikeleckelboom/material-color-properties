0eebfd00fd2c0c6fba94534e71f4f3a8ab45da6a
Maikel Eckelboom <maikeleckelboom@outlook.com>
2023-02-25 05:19:39 +0100

    trying something

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from '@importantimport/material-color-utilities'
 export const useMaterialColorProperties = (theme: any) => {
     console.log('🚀 useMaterialColorProperties called from index.ts')
 }
